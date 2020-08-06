4b438067e7177030cc54f2ad1b8e24964c455479
nisayana <nisayana23@yandex.ru>
2020-08-06 16:40:51 -0400

    4:39pm

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,25 @@
+require "pry"
 class Student
 
+    attr_accessor :first_name, :instructor, :percentage
+
+    @@all = []
+
+    def initialize(first_name, instructor, percentage)
+        @first_name = first_name
+        @instructor = instructor
+        @percentage = percentage
+        @@all << self
+
+    end
+
+    def self.all
+        # binding.pry
+        @@all
+    end
+
+    def add_boating_test(student, test_name, status, instructor)
+
+    end
 end
+
